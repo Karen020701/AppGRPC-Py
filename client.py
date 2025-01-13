4eb1aeca3c31c2c25b0adcffd570134c58b817a6
Karen020701 <karenchicaiza989@gmail.com>
2025-01-13 00:59:13 -0500

    first commit

---
+++ client.py
@@ -0,0 +1,16 @@
+import grpc
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = helloworld_pb2_grpc.GreeterStub(channel)
+    
+
+    response = stub.SayHello(helloworld_pb2.HelloRequest(name="Mundo"))
+    
+    print(f"Respuesta del servidor: {response.message}")
+
+if __name__ == '__main__':
+    run()
